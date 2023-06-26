81e65793fa552b812bbd2539f0eea8bee4a34c19
Mukesh Rao Nagendrarao <mukeshrao.nagendrarao@cesltd.com>
2023-06-26 20:14:27 +0530

    error handling

--- src/Components/NormalComponent.jsx
+++ src/Components/NormalComponent.jsx
@@ -4,14 +4,21 @@ import { useEffect, useState } from "react";
 const NormalComponent = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:4000/books").then((res) => setData(res.data));
+    axios
+      .get("http://localhost:4000/books")
+      .then((res) => setData(res.data))
+      .catch((error) => setError(error.message));
     setIsLoading(false);
   }, []);
 
   if (isLoading) {
     return <h5>Loading....</h5>;
+  }
+  if (error) {
+    return <h5>{error}</h5>;
   }
   return (
     <div>
--- src/Components/ReactQueryComponent.jsx
+++ src/Components/ReactQueryComponent.jsx
@@ -5,10 +5,13 @@ const ReactQueryComponent = () => {
   const fetchBooksApi = () => {
     return axios.get("http://localhost:4000/books");
   };
-  const { isLoading, data } = useQuery("books", fetchBooksApi);
+  const { isLoading, data, isError, error } = useQuery("books", fetchBooksApi);
 
   if (isLoading) {
     return <h5>Loading....</h5>;
+  }
+  if (isError) {
+    return <h5>{error.message}</h5>;
   }
   return (
     <div>
